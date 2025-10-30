fdb777cf8542818b5aba5be19e2d9c5976016a86
Your Name <youremail@example.com>
2025-10-30 09:17:38 +0530

    tailwind setup

--- src/App.js
+++ src/App.js
@@ -3,21 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen flex items-center justify-center bg-gray-900 text-white text-3xl font-bold">
+      Hello, Gotham 👋
     </div>
   );
 }
